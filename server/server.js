7db25c4bfd0fa1cc404d0231be7f04946b884a6f
Isuru maduranga <isurumaduranga.official@gmail.com>
2018-08-17 19:08:43 +0530

    Adding users

--- server/server.js
+++ server/server.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
 
 const Todo = mongoose.model('Todo',{
-    text:{type: String},
-    completed:{type:Boolean},
-    completedAt:{type:Number}
+    text:{type:String, required:true, minlength:1, trim:true},
+    completed:{type:Boolean, default:false},
+    completedAt:{type:Number, default:null}
+})
+
+const User =  mongoose.model('User',{
+    name:{type:String,required:true,minlength:1,trim:true},
+    email:{type:String,required:true,minlength:1,trim:true}
 })
 
 let todo = new Todo({
@@ -17,4 +22,14 @@ let todo = new Todo({
 todo.save().then(
     doc=>console.log('Saved todo',doc),
     e=>console.log('Uanble to save todo')
+)
+
+let user = new User({
+    name:'Isuru Maduranga',
+    email:'isurumaduranga.official@gmail.com'
+})
+
+user.save().then(
+    doc=>console.log('Saved todo',doc),
+    e=>console.log('Uanble to save user')
 )
