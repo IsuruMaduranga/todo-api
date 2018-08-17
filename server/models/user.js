48975268608246c4d06b6fe57e38d6d1debedfc7
Isuru maduranga <isurumaduranga.official@gmail.com>
2018-08-17 19:50:41 +0530

    Refactering

---
+++ server/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const User =  mongoose.model('User',{
+    name:{type:String,required:true,minlength:1,trim:true},
+    email:{type:String,required:true,minlength:1,trim:true}
+})
+
+module.exports({
+    User
+})
--- server/server.js
+++ server/server.js
@@ -1,35 +1,3 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
-
-const Todo = mongoose.model('Todo',{
-    text:{type:String, required:true, minlength:1, trim:true},
-    completed:{type:Boolean, default:false},
-    completedAt:{type:Number, default:null}
-})
-
-const User =  mongoose.model('User',{
-    name:{type:String,required:true,minlength:1,trim:true},
-    email:{type:String,required:true,minlength:1,trim:true}
-})
-
-let todo = new Todo({
-    text: 'Go shopping',
-    completed: false,
-    completedAt: 894949
-})
-
-todo.save().then(
-    doc=>console.log('Saved todo',doc),
-    e=>console.log('Uanble to save todo')
-)
-
-let user = new User({
-    name:'Isuru Maduranga',
-    email:'isurumaduranga.official@gmail.com'
-})
-
-user.save().then(
-    doc=>console.log('Saved todo',doc),
-    e=>console.log('Uanble to save user')
-)
+const {mongoose} = require('./db/mongoose')
+const {User} = require('./models/user')
+const {Todo} = require('./models/todo')
