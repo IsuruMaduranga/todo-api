3ce5d0f8ffb860d0b433dfcba11870c1a25ce42e
Isuru maduranga <isurumaduranga.official@gmail.com>
2018-08-21 23:36:20 +0530

    add web tokens to user

--- server/server.js
+++ server/server.js
@@ -94,9 +94,13 @@ app.patch('/todos/:id',(req,res)=>{
 app.post('/users',(req,res)=>{
     const body =  _.pick(req.body,['email','password'])
     const user = new User(body)
-    user.save().then(user=>{
-        res.send(user)
-    }).catch(e=>res.status(400).send(e))
+
+    user.save().then(()=>{
+        return user.generateAuthToken()
+    }).then((token)=>{
+        res.header('x-auth',token).send(user)
+    })
+    .catch(e=>res.status(400).send(e))
     
 })
 
