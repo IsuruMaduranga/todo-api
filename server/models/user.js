0d0d8da22843d817503db0f4ba8b94e9d9add0dd
Isuru maduranga <isurumaduranga.official@gmail.com>
2018-08-28 17:34:58 +0530

    add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -59,6 +59,17 @@ UserSchema.methods.generateAuthToken = function(){
     })
 }
 
+UserSchema.methods.removeToken =  function(token){
+    const user = this
+    return user.update({
+        $pull:{
+            tokens:{
+                token: token
+            }
+        }
+    })
+}
+
 UserSchema.statics.findByToken =  function(token){
     const User = this
     let decoded
